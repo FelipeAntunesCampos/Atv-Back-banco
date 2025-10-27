f9b93f586043e89c5c2fa4ae4dbd579e600d6d9a
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-10-27 15:39:48 -0300

    feat: Finaliza atv

--- src/models/PetModel.js
+++ src/models/PetModel.js
@@ -3,15 +3,43 @@ const prisma = new PrismaClient();
 
 //crio a variavel findAll e já exporto
 export const findAll = async () => {
-    return await prisma.Petshop.findMany({
+    return await prisma.petshop.findMany({
         orderBy: { nome: 'asc' }
     });
 }
 
 //crio a variavel findbyid e já exporto
 export const findbyid = async (id) => {
-    return await prisma.Petshop.findUnique({
+    return await prisma.petshop.findUnique({
         where: { id: Number(id)}
     });
 }
 
+export const create = async (data) =>   {
+    return await prisma.petshop.create({
+        data: {
+            nome: data.nome,
+            especie: data.especie,
+            idade: data.idade,
+            dono: data.dono,
+        }
+    })
+}
+
+export const deletePet = async (id) => {
+    return await prisma.petshop.delete({
+        where: { id: Number(id) }
+    })
+}
+
+export const update = async (id, data) => {
+    return await prisma.petshop.update({
+        where: { id: Number(id) },
+        data: {
+            ...(data.nome && { nome: data.nome }),
+            ...(data.especie && { especie: data.especie }),
+            ...(data.idade && { idade: data.idade }),
+            ...(data.dono && { dono: data.dono }),
+        }
+    })
+}
--- src/routes/PetRoute.js
+++ src/routes/PetRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 //rota Get all
 router.get("/", PetController.listarTodos);
 router.get("/:id", PetController.listarUm);
-
+router.post("/", PetController.criar);
+router.delete("/:id", PetController.apagar);
+router.put("/:id", PetController.atualizar);
 export default router;
